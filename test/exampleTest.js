cf9a6a29d0955bb5207f733c2c1ef41de9792284
Stoic Citium <vankhoa0603@gmail.com>
2022-09-17 06:35:08 +0700

    update sample

--- test/exampleTest.js
+++ test/exampleTest.js
@@ -1,9 +1,9 @@
 const { assert } = require('chai');
 const { utils } = require('@aeternity/aeproject');
 
-const EXAMPLE_CONTRACT_SOURCE = './contracts/ExampleContract.aes';
+const CONTRACT_SOURCE = './contracts/DeMarketly.aes';
 
-describe('ExampleContract', () => {
+describe('DeMarketly contract', () => {
     let aeSdk;
     let contract;
 
@@ -11,10 +11,10 @@ describe('ExampleContract', () => {
         aeSdk = await utils.getSdk();
 
         // a filesystem object must be passed to the compiler if the contract uses custom includes
-        const fileSystem = utils.getFilesystem(EXAMPLE_CONTRACT_SOURCE);
+        const fileSystem = utils.getFilesystem(CONTRACT_SOURCE);
 
         // get content of contract
-        const source = utils.getContractContent(EXAMPLE_CONTRACT_SOURCE);
+        const source = utils.getContractContent(CONTRACT_SOURCE);
 
         // initialize the contract instance
         contract = await aeSdk.getContractInstance({ source, fileSystem });
@@ -29,15 +29,19 @@ describe('ExampleContract', () => {
         await utils.rollbackSnapshot(aeSdk);
     });
 
-    // it('ExampleContract: set and get', async () => {
-    //   const set = await contract.methods.set(42, { onAccount: utils.getDefaultAccounts()[1] });
-    //   assert.equal(set.decodedEvents[0].name, 'SetXEvent');
-    //   assert.equal(set.decodedEvents[0].args[0], await utils.getDefaultAccounts()[1].address());
-    //   assert.equal(set.decodedEvents[0].args[1], 42);
+    it('Create product', async() => {
+        // const createProduct = await contract.methods.create_product(["id", "name", 100000, "description", "image", true], { onAccount: utils.getDefaultAccounts()[1] });
+        // const createProduct = await contract.methods.create_product("id", "name", 100000, "description", "image", true);
+        // console.log(createProduct)
 
-    //   const { decodedResult } = await contract.methods.get();
-    //   assert.equal(decodedResult, 42);
-    // });
+
+        //   assert.equal(set.decodedEvents[0].name, 'SetXEvent');
+        //   assert.equal(set.decodedEvents[0].args[0], await utils.getDefaultAccounts()[1].address());
+        //   assert.equal(set.decodedEvents[0].args[1], 42);
+
+        //   const { decodedResult } = await contract.methods.get();
+        assert.equal(42, 42);
+    });
 
     // it('ExampleContract: get undefined when not set before', async () => {
     //   const { decodedResult } = await contract.methods.get();
