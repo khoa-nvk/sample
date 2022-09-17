780b7c5a3c67820dbd5a24b3c792454a54d902cb
Khoa Nguyen (Stoic) <vankhoa0603@gmail.com>
2022-09-17 07:15:42 +0700

    Update exampleTest.js

--- test/exampleTest.js
+++ test/exampleTest.js
@@ -6,6 +6,17 @@ const CONTRACT_SOURCE = './contracts/DeMarketly.aes';
 describe('DeMarketly contract', () => {
     let aeSdk;
     let contract;
+    const assertNode = require('assert').strict;
+    
+    const accounts = utils.getDefaultAccounts();
+    const ownerAccount = accounts[0] 
+    const nonOwnerAccount = accounts[1]
+
+    const PRODUCT_ID_1 = "p1";
+    const PRODUCT_ID_2 = "p2";
+    const COUPON_50_OFF = "OFF50"
+    const PRODUCT_ID_INACTIVE = "inactive_product";
+    const REVIEW_ID = "review1";
 
     before(async() => {
         aeSdk = await utils.getSdk();
@@ -30,21 +41,39 @@ describe('DeMarketly contract', () => {
     });
 
     it('Create product', async() => {
-        // const createProduct = await contract.methods.create_product(["id", "name", 100000, "description", "image", true], { onAccount: utils.getDefaultAccounts()[1] });
-        // const createProduct = await contract.methods.create_product("id", "name", 100000, "description", "image", true);
-        // console.log(createProduct)
-
-
-        //   assert.equal(set.decodedEvents[0].name, 'SetXEvent');
-        //   assert.equal(set.decodedEvents[0].args[0], await utils.getDefaultAccounts()[1].address());
-        //   assert.equal(set.decodedEvents[0].args[1], 42);
-
-        //   const { decodedResult } = await contract.methods.get();
-        assert.equal(42, 42);
+        const createProduct = await contract.methods.create_product(PRODUCT_ID_1, "name", 100000, "description", "image", true);
+        assert.equal(createProduct.decodedResult, PRODUCT_ID_1);
     });
 
-    // it('ExampleContract: get undefined when not set before', async () => {
-    //   const { decodedResult } = await contract.methods.get();
-    //   assert.equal(decodedResult, undefined);
-    // });
+    it(`Create product fail because using the exist product's ID`, async() => {
+        const createProduct = await contract.methods.create_product(PRODUCT_ID_1, "name", 100000, "description", "image", true);
+        await assertNode.rejects(contract.methods.create_product(PRODUCT_ID_1, "name", 100000, "description", "image", true), (err) => {
+            assert.include(err.message, "Product's id is exist");
+            return true;
+        });
+    });
+
+    it(`Update product fail because of not product owner`, async() => {
+        const createProduct = await contract.methods.create_product(PRODUCT_ID_1, "name", 100000, "description", "image", true, {onAccount: ownerAccount});
+        await assertNode.rejects(contract.methods.update_product(PRODUCT_ID_1, "name", 100000, "description", "image", true, {onAccount: nonOwnerAccount}), (err) => {
+            assert.include(err.message, "You are not the product's owner");
+            return true;
+        }); 
+    });
+
+    it(`Update product fail because of not found product id`, async() => {
+        const createProduct = await contract.methods.create_product(PRODUCT_ID_1, "name", 100000, "description", "image", true, {onAccount: ownerAccount});       
+        await assertNode.rejects(contract.methods.update_product(PRODUCT_ID_2, "name", 100000, "description", "image", true, {onAccount: ownerAccount}), (err) => {
+            assert.include(err.message, "Maps: Key does not exist");
+            return true;
+        }); 
+    });
+
+    it(`Update product success`, async() => {
+        const createProduct = await contract.methods.create_product(PRODUCT_ID_1, "name", 100000, "description", "image", true, {onAccount: ownerAccount});
+        const updateProduct = await contract.methods.update_product(PRODUCT_ID_1, "name 2", 100000, "description", "image", true, {onAccount: ownerAccount});       
+        assert.equal(createProduct.decodedResult, PRODUCT_ID_1);
+    });
+
+   
 });
